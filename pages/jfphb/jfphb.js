e257156b3db2ca4442b4be48332249adb2d4fc43
daimeichao <853637807@qq.com>
2023-02-16 18:05:50 +0800

    商城

--- pages/jfphb/jfphb.js
+++ pages/jfphb/jfphb.js
@@ -1,102 +1,78 @@
-// pages/jfphb/jfphb.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    phblist:[],
-    total:''
-
+  data: {},
+  onLoad: function(options) {
+    // 页面初始化 options为页面跳转所带来的参数
+    this.tempData();
   },
-  getDetail(){
-    var that =this 
-    // use api
-    wx.request({
-      url: app.globalData.webroot + 'xcx/api/getphb', //上线的话必须是https，没有appId的本地请求貌似不受影响 
-      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT 
-      header: {
-        "Content-Type": "application/json",
-      }, // 设置请求的 header
-      data: {
-        pageindex: 1,
-        pagesize: that.data.pageSize,
+  //测试临时数据
+  tempData: function() {
+    var list = [{
+        rank: "1",
+        txtStyle: "",
+        icon: "/images/my.png",
+        name: "李飞",
+        pace: "23456",
       },
-      success: function (res) {
-        console.log("排行榜res",res)
-        let result = res.data.data
-        if(result.resultCode == 2000){ // 成功
-          let resultList =  result.wishList
-          that.setData({
-            phblist: resultList,
-            total: result.total
-          })
-          if(resultList === null || resultList.length===0){
-            that.setData({
-              showText: '暂无数据',
-            })
-          }
-  
-        }else{ // 失败
-          app.toast(result.message)
-        }
+      {
+        rank: "2",
+        txtStyle: "",
+        icon: "/images/my.png",
+        name: "张叶",
+        pace: "23450",
       },
-    })
-  },
+      {
+        rank: "3",
+        txtStyle: "",
+        icon: "/images/my.png",
+        name: "王小婷",
+        pace: "22345",
+      },
+      {
+        rank: "4",
+        txtStyle: "",
+        icon: "/images/my.png",
+        name: "袁经理",
+        pace: "21687",
+      },
+      {
+        rank: "5",
+        txtStyle: "",
+        icon: "/images/my.png",
+        name: "陈雅婷",
+        pace: "21680",
+      },
+      {
+        rank: "6",
+        txtStyle: "",
+        icon: "/images/my.png",
+        name: "许安琪",
+        pace: "20890",
+      },
+      {
+        rank: "7",
+        txtStyle: "",
+        icon: "/images/my.png",
+        name: "里俊飞",
+        pace: "20741",
+      },
+      {
+        rank: "8",
+        txtStyle: "",
+        icon: "/images/my.png",
+        name: "李小俊",
+        pace: "19511",
+      },
+      {
+        rank: "9",
+        txtStyle: "",
+        icon: "/images/my.png",
+        name: "陈俊飞",
+        pace: "19501",
+      },
+    ]
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+    this.setData({
+      list: list
+    });
   }
 })
